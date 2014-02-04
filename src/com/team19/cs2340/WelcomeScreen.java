f55d4466e8915a69b35a70d76d863797d1ad77c1
Stephan Williams <stephanwilliams.93@gmail.com>
2014-02-04 03:06:13 -0500

    Completed milestone 3
    wow so much code

--- src/com/team19/cs2340/WelcomeScreen.java
+++ src/com/team19/cs2340/WelcomeScreen.java
@@ -1,8 +1,12 @@
 package com.team19.cs2340;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreen extends Activity {
 
@@ -10,6 +14,16 @@ public class WelcomeScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        
+        Button login = (Button)findViewById(R.id.login);
+        login.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				Intent intent = new Intent(WelcomeScreen.this, LogInActivity.class);
+				startActivity(intent);
+				return true;
+			}
+        });
     }
 
 
