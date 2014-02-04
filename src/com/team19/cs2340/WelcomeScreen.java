0162161fe2eb150f065a3012234ce0c4131d4df2
Stephan Williams <stephanwilliams.93@gmail.com>
2014-02-04 01:41:28 -0500

    creating base project

---
+++ src/com/team19/cs2340/WelcomeScreen.java
@@ -0,0 +1,23 @@
+package com.team19.cs2340;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class WelcomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.welcome_screen, menu);
+        return true;
+    }
+    
+}
